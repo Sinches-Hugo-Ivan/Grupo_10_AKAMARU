3f435faf300405b9032ce3722d130286e639e76c
GabrielAmestoy <gabrielamestoy1993@gmail.com>
2021-08-12 00:58:00 -0300

    update rutas y app Sprint 4

--- sitio/src/routes/products.js
+++ sitio/src/routes/products.js
@@ -1,16 +1,35 @@
+const path = require('path');
 var express = require('express');
 var router = express.Router();
-const {add,detail, search,save,edit,update,remove} = require('../controllers/productsController');
 
+
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+    destination : (req, file, cb) =>{
+        cb(null, "public/images")
+    },
+    filename : (req,file,cb) =>{
+        cb(null,file.fieldname + "-" + Date.now() + path.extname(file.originalname))
+    }
+})
+const upload = multer({
+    storage,
+})
+
+const {add,detail, search,save,edit,update,remove, productos,vistaAdmin} = require('../controllers/productsController');
 
 /* /products */
+router.get('/', productos);
 router.get('/Add',add);
-router.post('/add',save);
+router.post('/add',upload.single('image'), save);
 router.get('/detail/:id',detail);
 router.get('/search',search);
 router.get('/edit/:id',edit);
 router.put('/edit/:id',update);
+router.get('/vistaAdmin', vistaAdmin);
 router.delete('/remove/:id',remove);
+
 
 
 module.exports = router;
