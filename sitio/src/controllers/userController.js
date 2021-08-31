47d269b48be3be9aff19b079664c4ecbfd91cf55
MariaDominguez8 <maria.e.dominguez.97@gmail.com>
2021-08-30 23:50:20 -0300

    login

--- sitio/src/controllers/userController.js
+++ sitio/src/controllers/userController.js
@@ -1,3 +1,7 @@
+const bcrypt = require('bcryptjs');
+const { validationResult } = require('express-validator');
+const {productos} = require('../data/products_db');
+
 module.exports = {
     register: (req, res) => {
         return res.render('register')
@@ -6,7 +10,24 @@ module.exports = {
     login: (req, res) => {
         return res.render('login')
     },
+    processLogin: (req,res) =>{
+        let errores = validationResult(req);
+        const {email, password} = req.body;
+        if(errores.isEmpty()){
+            let usuario = usuarios.find(usuario => usuario.email == email)
+            req.session.userLogin = {
+            id : usuario.id,    
+            name : usuario.firstName,
+            }
+            return res.redirect('/');
+        }else{
+            return res.render('login',{
+                productos,
+                errores : errores.mapped()
+            } )
+        }
 
+    },
     cart: (req, res) => {
         return res.render('cart')
     }
--- sitio/src/routes/users.js
+++ sitio/src/routes/users.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var router = express.Router();
-const {login,register,cart} = require('../controllers/userController');
+const {login,register,cart,processLogin} = require('../controllers/userController');
+const loginValidator = require('../validations/loginValidator');
 
 
 /* GET users listing. */
 router.get('/login', login);
+router.post('/login',loginValidator,processLogin);
 router.get('/register', register);
 router.get('/cart', cart);
 
