e2e41259e296d60e96ee0adcc19e942b805d7aa1
MariaDominguez8 <maria.e.dominguez.97@gmail.com>
2021-08-30 23:39:50 -0300

    categorias

--- sitio/src/routes/products.js
+++ sitio/src/routes/products.js
@@ -17,17 +17,19 @@ const upload = multer({
     storage,
 })
 
-const {add,detail, search,save,edit,update,remove, productos,vistaAdmin} = require('../controllers/productsController');
+const {add,detail,search,save,edit,update,remove, productos,vistaAdmin,categoriasProduct} = require('../controllers/productsController');
 
 /* /products */
 router.get('/', productos);
-router.get('/Add',add);
-router.post('/add',upload.single('image'), save);
+router.get('/add',add);
+router.post('/add',upload.array('images'), save);
 router.get('/detail/:id',detail);
 router.get('/search',search);
 router.get('/edit/:id',edit);
 router.put('/edit/:id',update);
 router.get('/vistaAdmin', vistaAdmin);
+router.get('/categorias', categoriasProduct);
+router.get('/categorias/:title', categoriasProduct);
 router.delete('/remove/:id',remove);
 
 
