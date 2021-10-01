443d81c466f9cab25d0e4bb5d58cba59f5979c03
GabrielAmestoy <gabrielamestoy1993@gmail.com>
2021-10-01 18:39:49 -0300

    add previsualizacion de imagen al editar foto perfil

--- sitio/src/middlewares/adminUserCheck.js
+++ sitio/src/middlewares/adminUserCheck.js
@@ -1,4 +1,6 @@
 module.exports = (req,res,next) => {
+    console.log("---------");
+    console.log(req.session.userLogin);
     if(req.session.userLogin && req.session.userLogin.rol === "admin"){
         next();
     }else{
